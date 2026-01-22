a12c38922077ad150a9c8b70d1b0c313c5e6a9c5
bipinstha07 <shresthab2@warhawks.ulm.edu>
2026-01-22 11:57:25 -0600

    Updated Controller

--- Server/src/main/java/com/grade/forge/ForgeApplication.java
+++ Server/src/main/java/com/grade/forge/ForgeApplication.java
@@ -11,20 +11,7 @@ import java.sql.Connection;
 public class ForgeApplication {
 
 	public static void main(String[] args) {
-
-		ConfigurableApplicationContext context =
 				SpringApplication.run(ForgeApplication.class, args);
 
-		try {
-			DataSource dataSource = context.getBean(DataSource.class);
-			try (Connection connection = dataSource.getConnection()) {
-				System.out.println("✅ PostgreSQL connection SUCCESS");
-				System.out.println("Connected to: " +
-						connection.getMetaData().getURL());
-			}
-		} catch (Exception e) {
-			System.err.println("❌ PostgreSQL connection FAILED");
-			e.printStackTrace();
-		}
 	}
 }
--- Server/src/main/java/com/grade/forge/controller/Testing.java
+++ Server/src/main/java/com/grade/forge/controller/Testing.java
@@ -1,22 +1,36 @@
 package com.grade.forge.controller;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+
 @RestController
 @RequestMapping("/test")
 public class Testing {
 
+    // Inject the DataSource bean
+    @Autowired
+    private DataSource dataSource;
 
-    @GetMapping()
-    public ResponseEntity<String> testing(){
-        return new ResponseEntity<>("Testing", HttpStatus.OK);
+    @GetMapping
+    public ResponseEntity<String> testing() {
+        String data;
+
+        try (Connection connection = dataSource.getConnection()) {
+            System.out.println("✅ PostgreSQL connection SUCCESS");
+            data = connection.getMetaData().getURL();
+        } catch (Exception e) {
+            System.err.println("❌ PostgreSQL connection FAILED");
+            e.printStackTrace();
+            return new ResponseEntity<>("Connection FAILED", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<>("Connection SUCCESS "+ data, HttpStatus.OK);
     }
-
-
-
 }
