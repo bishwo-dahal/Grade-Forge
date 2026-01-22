391c644f2eab2a726b13c25b8d9ab34aadc6dbb6
bishwo-dahal <bishwodahal123@gmail.com>
2026-01-22 12:02:13 -0600

    removed unused dependencies

--- Server/src/main/java/com/grade/forge/ForgeApplication.java
+++ Server/src/main/java/com/grade/forge/ForgeApplication.java
@@ -2,10 +2,6 @@ package com.grade.forge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
 
 @SpringBootApplication
 public class ForgeApplication {
